a46fccf3c6359c86757ec031f653bd3ed34c747b
2or3 <t.nagashima.cog@gmail.com>
2013-08-03 02:40:30 +0900

    fix sound

--- main.lua
+++ main.lua
@@ -14,7 +14,12 @@ piano.height = piano.height * 2
 
 piano.x = display.contentWidth / 2
 piano.y = display.contentHeight - piano.height / 2
-physics.addBody( piano, "static", { friction=3 } )
+physics.addBody( piano, "static", { isSensor=true } )
+
+local land = display.newRect( 0, display.contentHeight, display.contentWidth, 1 )
+physics.addBody( land, "static", { friction=0.2 } )
+
+local doSound = audio.loadSound("sound/do.mp3")
 
 local onTouch = function( event )
   if event.phase == "began" then
@@ -22,8 +27,13 @@ local onTouch = function( event )
     yuki.x, yuki.y = event.x, event.y
     yuki.width, yuki.height = yuki.width / 15, yuki.height / 15
     yuki.rotation = 50
-    physics.addBody( yuki, { bounce=0.3 } )
+    physics.addBody( yuki, { bounce=0.2 } )
     return true
   end
 end
 Runtime:addEventListener( "touch", onTouch )
+
+local onCollision = function()
+  audio.play( doSound )
+end
+piano:addEventListener( "collision", onCollision )
